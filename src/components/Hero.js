93156db23ad707fc1ce132729b1aabd91dfd8718
Elias Symons <eisymonsmusic@gmail.com>
2023-04-04 10:31:11 -0700

    updated hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,12 +7,11 @@ function Hero() {
       className="bg-[url('./mobile-bg.jpg')] bg-black bg-opacity-60 bg-blend-overlay   bg-center object-cover flex-col items-start space-y-6 justify-start text-center  text-[#EFF6E0] pt-[20vh] pb-[20vh] md:space-y-10 md:bg-[url('./hero-bg.jpg')] "
     >
       <h1 className=" text-3xl font-extrabold max-w-xs mx-auto md:max-w-xl leading-[3.5rem] md:text-4xl  lg:text-5xl uppercase lg:max-w-2xl">
-        Modern Web Solutions
+        DEVTAILS
       </h1>
       <div className="space-y-10 text-center">
-        <p className="text-xl max-w-xs mx-auto md:max-w-sm md:text-2xl">
-          Using the latest technologies to create responsive and efficient web
-          applications.
+        <p className="text-xl font-bold max-w-xs mx-auto md:max-w-sm md:text-2xl">
+          Modern solutions to modern web needs.
         </p>
         <Link
           to="contact"
